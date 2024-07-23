b871f1001812e32abd4275f090b4b5db53a2684a
Name <muhammad.saqlain.0978@gmail.com>
2024-07-23 14:10:51 +0500

    added 3files

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+let bx = document.querySelector("#box1");
+ 
+//  bx.onclick = () => {
+//     bx.innerText = "sdf";
+//     bx.style.color= "green";
+//     bx.document.style.bac
+    
+   
+    
+    
+    
+
+//  }
+
+ bx.addEventListener("click", ()=>{
+   console.log("sdf");
+ })
